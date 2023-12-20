79535366ccf4003550bee859dd5310b14f6058de
arinl1204 <143991629+arinl1204@users.noreply.github.com>
2023-12-20 22:06:35 +0900

    IMD232_FIN_202220325

--- script/sketch.js
+++ script/sketch.js
@@ -1,47 +1,6 @@
-// let angle = 0;
-// let randomColor;
-
-// function setup() {
-//   setCanvasContainer('canvas', 1, 1, true);
-//   rectMode(CENTER);
-//   randomColor = color(random(360));
-// }
-
-// function draw() {
-//   background(0);
-
-//   for (let i = 50; i < mouseX; i += 50) {
-//     for (let j = 50; j < mouseY; j += 50) {
-//       // noFill();
-//       // fill(100, random(360), random(360), 25);
-//       fill(randomColor);
-
-//       noStroke();
-//       stroke(random(360), random(360), random(360), 90);
-//       strokeWeight(3);
-
-//       let d = dist(mouseX, mouseY, i + width / 2, j + height / 2);
-
-//       let r = d / 5;
-//       push();
-//       translate(i, j);
-//       rotate(angle);
-//       // ellipse(0, 0, r, r + 20);
-//       rect(0, 0, r, r);
-//       pop();
-
-//       angle += 0.0003;
-//     }
-//   }
-// }
-
-// function mousePressed() {
-//   randomColor = color(100, random(360), random(360), 25);
-// }
-
 let angle = 0;
 let randomColor;
-let Shape = 'ellipse'; // 초기 모양을 타원으로 설정
+let Shape = 'ellipse';
 
 function setup() {
   setCanvasContainer('canvas', 1, 1, true);
@@ -93,3 +52,5 @@ function mousePressed() {
     Shape = 'ellipse';
   }
 }
+
+//https://youtu.be/EMIoUxLEYJ4?si=Vu7F4HB6oO6eNt2Y 메인코드
